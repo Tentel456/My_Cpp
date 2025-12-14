b9e82f464f7a3100b071c1c787f2feafd26ed64b
Tentel456 <126483734+Tentel456@users.noreply.github.com>
2025-12-14 15:51:03 +0300

    1.1

---
+++ 1/1.1/main.cpp
@@ -0,0 +1,11 @@
+//Cout Training with different types
+#include <iostream>
+
+int main()
+{
+    std::cout << 5;
+    std::cout << -9.5;
+    std::cout << 'A';
+    std::cout << "Hello, World";
+    return 0;
+}
