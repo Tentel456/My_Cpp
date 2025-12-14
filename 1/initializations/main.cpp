6b45b6e5cd3d224e00a565ebf0ad8df9879af400
Tentel456 <126483734+Tentel456@users.noreply.github.com>
2025-12-14 18:26:48 +0300

    init training

---
+++ 1/initializations/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a {15};
+    double b {17};
+    char c = '$';
+    bool is_looged_in = true;
+    cout << "Баланс:" << a << "из" << b << c << is_looged_in;
+    return 0;
+    }
