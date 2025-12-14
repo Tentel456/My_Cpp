4d81e24d1428cd16dc39276c4d19c7201c56864c
Tentel456 <126483734+Tentel456@users.noreply.github.com>
2025-12-14 15:53:24 +0300

    Create main.cpp

---
+++ 1/1.2/main.cpp
@@ -0,0 +1,13 @@
+//Тренировка для типов данных
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double a, b; //Initializing current and total balances, with X.XX Type
+    char Dollar = '$';
+    a = 15.75; //Current balance
+    b = 20.85; //Total balance
+    cout << "У вас осталось" << a << Dollar << "из" << b << Dollar;
+    return 0;
+}
