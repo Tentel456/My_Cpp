8bdfdfc5b78a2285e85cd1c446ac81c44d59e3a9
Tentel456 <126483734+Tentel456@users.noreply.github.com>
2025-12-20 14:10:59 +0300

    Add basic input/output for three numbers
    This program prompts the user to enter three numbers and displays them.

---
+++ 4/3/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    int x {};
+    int y {};
+    int z {};
+    std::cout << "Введите три числа:";
+    std:: cin >> x >> y >> z;
+    std::cout << "Введенные числа:" << x << y << z;
+    return 0;
+}
