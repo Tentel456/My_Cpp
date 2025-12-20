21b16a5fc997589cbc03066c58dc9d0ed0393406
Tentel456 <126483734+Tentel456@users.noreply.github.com>
2025-12-20 13:45:08 +0300

    Create main.cpp

---
+++ 4/1/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    int x {};
+    std::cout << "Введите X" << "\n";
+    std::cin >> x;
+    std::cout << "X равен:" << x << "\n";
+    std::cout << "Вы ввели это значение с клавиатуры!";
+    return 0;
+}
