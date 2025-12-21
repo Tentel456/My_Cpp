ea5bae7f463dc33abc0f0d1eec370db9f78a3359
Tentel456 <126483734+Tentel456@users.noreply.github.com>
2025-12-21 16:02:01 +0300

    Create main.cpp

---
+++ 5/4/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int x {}; //инициализируем первую операнду
+    int y {}; //инициализируем вторую операнду
+    std::cin >> x;
+    std::cin >> y;
+    std::cout << x + y; // производим операцию
+    return 0;
+}
