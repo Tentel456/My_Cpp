048edcaab133410e5b7531eb32e2eb1ba0c39a5f
Tentel456 <126483734+Tentel456@users.noreply.github.com>
2026-01-01 00:17:37 +0300

    Create bank.cpp

---
+++ 10/1/bank.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int bank()
+{
+	std::cout << "Welcome to the bank!" << "\n";
+	int balance {0};
+	std::cout << "Your balance:" << balance << "\n";
+	int deposit {};
+	return 0;
+}
