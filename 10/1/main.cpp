daf11cfc09cdfa55e9c3fc3f95376d6807f8e7c7
Tentel456 <126483734+Tentel456@users.noreply.github.com>
2026-01-01 00:14:55 +0300

    Create main.cpp

---
+++ 10/1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+
+int bank();
+int deposit(int balance);
+int investments();
+
+int main()
+{
+	bank();
+	deposit(0);
+	investments();
+	return EXIT_SUCCESS;
+}
