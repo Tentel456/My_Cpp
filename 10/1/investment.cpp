6ef2ac57f18bfab848256aaa35615cf8836d88b1
Tentel456 <126483734+Tentel456@users.noreply.github.com>
2026-01-01 00:16:28 +0300

    Create investment.cpp

---
+++ 10/1/investment.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int investments()
+{
+	std::cout << "Доступные инвестиции\n";
+	std::cout << "1. Крипта\n";
+  	std::cout << "2. Акции\n";
+	std::cout << "3. Облигации\n";
+	std::cout << "4. Недвижимость\n";
+	std::cout << "В скорем можно будет выбрать одну из них!\n";
+	return 0;
+}
