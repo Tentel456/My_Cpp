ff65d8ca91696179551a21188b7b77abbd1b2779
Tentel456 <126483734+Tentel456@users.noreply.github.com>
2025-12-31 21:13:14 +0300

    Create main.cpp

---
+++ 9/1/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdlib>
+
+int add(int a, int b);
+
+int main() {
+	std::cout << add(10,10) << "\n";
+	std::cout << "Hello, World!" << "\n";
+	return EXIT_SUCCESS;
+}
