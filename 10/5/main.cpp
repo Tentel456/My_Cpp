4a933cd1b0364601c6b27f6c3e14874b0842a91f
Tentel456 <126483734+Tentel456@users.noreply.github.com>
2026-01-01 23:06:04 +0300

    Basic obfuscate with #define macros

---
+++ 10/5/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+
+#define x int
+#define xx main
+#define xxx {
+#define xxxx }
+#define xxxxx return
+#define xxxxxx EXIT_SUCCESS
+#define xxxxxxx std
+#define xxxxxxxx <<
+#define xxxxxxxxx ::
+#define xxxxxxxxxx cout
+#define xxxxxxxxxxx "Hello, world"
+#define xxxxxxxxxxxx ;
+
+
+x xx()
+xxx
+    xxxxxxx xxxxxxxxx xxxxxxxxxx xxxxxxxx xxxxxxxxxxx xxxxxxxxxxxx
+    xxxxx xxxxxx xxxxxxxxxxxx
+xxxx
