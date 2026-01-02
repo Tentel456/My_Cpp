e861c3d478c5d6625541da52fc3429d9b8c50727
Tentel456 <126483734+Tentel456@users.noreply.github.com>
2026-01-02 13:21:40 +0300

    Create main.cpp

---
+++ 11/2/main.cpp
@@ -0,0 +1,11 @@
+#include "crypto.h"
+#include <iostream>
+#include <cstdlib>
+
+xxxxxxx xxxxxxxx xx xxxx xxxxxxxxxx
+
+x xxx xx xxxx
+xxxxx
+    xxxxxxxx xx xxxx xxxxxxxxxx
+    xxxxxxxxxxx xxxxxxxxxxxx xxxxxxxxxx
+xxxxxx
