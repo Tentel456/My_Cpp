c6d8ca0a1ce8fa795758af37fe4aca291c422ac9
Tentel456 <126483734+Tentel456@users.noreply.github.com>
2026-01-02 13:22:27 +0300

    obfuscate with define

---
+++ 11/2/crypto.h
@@ -0,0 +1,16 @@
+#define x int
+#define xx (
+#define xxx main
+#define xxxx )
+#define xxxxx {
+#define xxxxxx }
+#define xxxxxxx void
+#define xxxxxxxx welcome
+#define xxxxxxxxxx ;
+#define xxxxxxxxxxx return
+#define xxxxxxxxxxxx EXIT_SUCCESS
+#define xxxxxxxxxxxxx std
+#define xxxxxxxxxxxxxx ::
+#define xxxxxxxxxxxxxxx cout
+#define xxxxxxxxxxxxxxxx <<
+#define xxxxxxxxxxxxxxxxx "Welcome to the bank!:)"
