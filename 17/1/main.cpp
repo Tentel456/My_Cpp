a3e1495154fe149ffa9ec7153798e9835db26911
Tentel456 <126483734+Tentel456@users.noreply.github.com>
2026-01-22 15:31:57 +0300

    add size

--- 17/1/main.cpp
+++ 17/1/main.cpp
@@ -7,5 +7,6 @@ int main()
 {
 	std::cout << "C++ Standard Version: " << __cplusplus << std::endl;
 	std::cout << "Size of bite" << CHAR_BIT << " bits" << std::endl;
+	std::cout << "Size of int" << sizeof(int) << " bytes" << std::endl;
 	return EXIT_SUCCESS;
 }
