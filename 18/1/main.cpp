94580cbcb6fe05c14183a17a056c00584206ceae
Tentel456 <126483734+Tentel456@users.noreply.github.com>
2026-04-05 11:27:15 +0300

    Update main.cpp

--- 18/1/main.cpp
+++ 18/1/main.cpp
@@ -1,10 +1,12 @@
 //КОД БУДЕТ ДОПОЛНЯТЬСЯ, МИНИ АПДЕЙТ - КОММИТ
 #include "welcome.h"
+#include "balance.h"
 #include <iostream>
 #include <cstdlib>
 
 int main()
 {
     welcome();
+    balance();
     return EXIT_SUCCESS;
 }
