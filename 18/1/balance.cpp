669ddc2b5fadc16b74c9592e1e3f5fc2e8e3d42d
Tentel456 <126483734+Tentel456@users.noreply.github.com>
2026-04-05 11:28:19 +0300

    Create balance.cpp

---
+++ 18/1/balance.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+void balance()
+{
+    //Инициализация баланса - начальное значение всегда равно нулю
+    int balance { 0 };
+    
+    std::cout << "Ваш баланс:" << " " << balance << "\n";
+    
+}
